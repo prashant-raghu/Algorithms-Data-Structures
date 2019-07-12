7a8512e7076e6e8e11283a642fc0bc86c5a31aac
prashantraghu999 <prashantraghu999@gmail.com>
2019-07-12 21:38:30 +0530

    Contests Init

---
+++ Template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+	cin.sync_with_stdio(0); cin.tie(0); //Comment while performing interactive IO
+	cin.exceptions(cin.failbit); // Logical error on i/o operation
+
+}
